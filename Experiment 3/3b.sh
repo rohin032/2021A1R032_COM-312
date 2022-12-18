a0f157a47e0a3e2d8393ec42521d43d16a8445c0
Rohin Sabharwal <100260827+rohin032@users.noreply.github.com>
2022-12-18 17:49:41 +0530

    Add files via upload

---
+++ Experiment 3/3b.sh
@@ -0,0 +1,11 @@
+# get filename
+echo -n "Enter File Name : "
+read filename
+# make sure file exists for reading
+if [ ! -f $filename ]
+then
+echo "Filename $fileName does not exists"
+exit 1
+fi
+# convert to uppercase using tr command
+tr '[a-z]' '[A-Z]' < $fileName
