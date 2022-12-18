4e6cc2d22b1f0e6d919b9596cad937cb64fe3b19
Rohin Sabharwal <100260827+rohin032@users.noreply.github.com>
2022-12-18 17:38:17 +0530

    Add files via upload

---
+++ Experiment 4/4b.sh
@@ -0,0 +1,9 @@
+echo "enter the filename"
+read fname
+echo "enter the starting line number"
+read s
+echo "enter the ending line number"
+read n
+sed -n $s,$n\p $fname | cat > 4c.txt
+cat 4c.txt 
+
